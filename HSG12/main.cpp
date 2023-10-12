d3266790d6f2cc4e82888d1e64b0c3c0eeb06fb4
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2023-10-12 15:01:28 +0700

    Create main.cpp

---
+++ HSG12/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define FOR(i,l,r) for (int i=l;i<=r;i++)
+#define FOD(i,r,l) for (int i=r;i>=l;i--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pub push_back
+#define pob pop_back
+#define ii pair<int,int>
+#define pll pair<long long, long long>
+#define F first
+#define S second
+typedef unsigned long long int ull;
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+    fast;
+    freopen(".inp","r",stdin);
+    freopen(".out","w",stdout);
+    solve();
+}
