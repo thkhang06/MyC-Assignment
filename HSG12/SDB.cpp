d9c8373c43291ac6453735f2702de7b0c87da30c
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2023-11-02 15:58:06 +0700

    Update SDB.cpp

--- HSG12/SDB.cpp
+++ HSG12/SDB.cpp
@@ -1,40 +1,78 @@
-#include <bits/stdc++.h>
-typedef long long ll;
-#define FOR(i,l,r) for (int i=l;i<=r;i++)
-#define FOD(i,r,l) for (int i=r;i>=l;i--)
-#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define pub push_back
-#define pob pop_back
-#define ii pair<int,int>
-#define pll pair<long long, long long>
-#define F first
-#define S second
-typedef unsigned long long int ull;
+#include<bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+
+using str = std::string;
+using ull = unsigned long long;
+using ii = std::pair <int, int>;
+
+#define X first
+#define Y second
+#define endl '\n'
+#define pb push_back
+#define int long long
+#define And << " " <<
+#define len(x) (long long)x.length()
+#define reset(a, x) memset(a, x, sizeof(a))
+#define End cout << '\n' << "\n"; return 0;
+#define Fu(i, l, r) for (int i = l; i <= r; i++)
+#define enter_arr(x, n) Fu(i, 1, n) cin >> x[i];
+#define File(x) freopen(x".INP", "r", stdin); freopen(x".OUT", "w", stdout);
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
-int demuoc(int n) {
-    int cnt = 0;
-    FOR(i,1,sqrt(n)){
-        if (n%i == 0) {
-            if (n/i == i) cnt++;
-            else cnt += 2;
-        }
+const int Max = 1e6;
+const int N = 1e6 + 5;
+
+/*-------------// Variables \\--------------*/
+    int a[N], d[N];
+/*------------------------------------------*/
+
+bool scp(int a)
+{
+    int b = sqrt(a);
+    return (b * b == a);
+}
+
+int cnt(int a)
+{
+    int ans = 0;
+    for(int i = 1; i * i <= a; i++)
+    {
+        if (a % i == 0) ans += 2;
+        if (ans > 10) break;
     }
-    return cnt;
+
+    ans -= scp(a);
+    return ans;
 }
 
-void solve(){
-    int a, b, kq = 0;
-    cin >> a >> b;
-    FOR(i,a,b) if (i%3 == 0 && demuoc(i) == 9) ++kq;
-    cout << kq << '\n';
+void precompute()
+{
+    for(int i = 1; i <= Max; i++)
+    {
+        if (!scp(i) || (i % 3 != 0)) continue;
+        a[i] = (cnt(i) == 9);
+    }
+
+    for(int i = 1; i <= Max; i++)
+        d[i] = d[i-1] + a[i];
 }
 
-int main(){
-    fast;
-    //freopen("SDB.inp","r",stdin);
-    //freopen("SDB.out","w",stdout);
-    int t;
-    cin >> t;
-    while (t--) solve();
+void solve()
+{
+    int l, r; cin >> l >> r;
+    cout << d[r] - d[l-1] << endl;
+}
+
+int32_t main()
+{
+    Fast;
+    File("SDB");
+    // freopen("test.inp", "r", stdin);
+
+     precompute();
+     int t; cin >> t; while(t--)
+        solve();
+    End;
 }
