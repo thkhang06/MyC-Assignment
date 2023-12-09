19df8d3546c678717068c8121d09e8d0fb7fb8b0
Thái Khang <82371312+thkhang06@users.noreply.github.com>
2023-12-09 09:21:09 +0700

    Update CNTNUM.cpp

--- HSG12/CNTNUM.cpp
+++ HSG12/CNTNUM.cpp
@@ -1,41 +1,35 @@
 #include <bits/stdc++.h>
 typedef long long ll;
-#define FOR(i,l,r) for (int i=l;i<=r;i++)
-#define FOD(i,r,l) for (int i=r;i>=l;i--)
+#define FOR(i, l, r) for (int i = l; i <= r; i++)
+#define FOD(i, r, l) for (int i = r; i >= l; i--)
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define pub push_back
 #define pob pop_back
-#define ii pair<int,int>
+#define ii pair<int, int>
 #define pll pair<long long, long long>
 #define F first
 #define S second
 typedef unsigned long long int ull;
 using namespace std;
+const int N = 1e6+5;
 
-double a,b,c,d;
+ll a, b, c, d;
 
-long long lcm(int a, int b)
-{
-    return (a / __gcd(a, b)) * b;
+ll bcnn(ll a, ll b) {
+    return a*b/__gcd(a,b);
 }
 
-int countt(int A, int B, int M)
-{
-    if (A % M == 0)
-        return (B / M) - (A / M) + 1;
-    return (B / M) - (A / M);
-}
-
-void solve(){
+void solve() {
     cin >> a >> b >> c >> d;
-    double kq = (b-a+1) - (countt(a,b,c) + countt(a,b,d) - countt(a,b,lcm(c,d)));
-    cout << kq;
-    //cout << floor((b)/lcm(c,d))-floor((a)/lcm(c,d));
+    ll xc = b/c - (a-1)/c;
+    ll xd = b/d - (a-1)/d;
+    ll xcd = b/bcnn(c,d) - (a-1)/bcnn(c,d);
+    cout << b-a+1 - (xc + xd - xcd);
 }
 
 int main(){
     fast;
-    //freopen("CNTNUM.inp","r",stdin);
-    //freopen("CNTNUM.out","w",stdout);
+    freopen("CNTNUM.inp","r",stdin);
+    freopen("CNTNUM.out","w",stdout);
     solve();
 }
